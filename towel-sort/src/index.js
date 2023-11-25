903389fde33bc89c15c19b98a4888d143bc891e8
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-25 14:56:00 +0300

    feat: add towel-sort task

---
+++ towel-sort/src/index.js
@@ -0,0 +1,9 @@
+module.exports = function towelSort (matrix) {
+	if (arguments.length === 0)
+		return [];
+
+	return matrix.reduce((result, curElement, i) => {
+		curElement.sort((a, b) => !(i % 2) ? a - b : b - a).map(element => result.push(element));
+		return result;
+	}, []);
+}

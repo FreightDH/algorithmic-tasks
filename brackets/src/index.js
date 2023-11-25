1106fbc56e37ddf7efdf406e12b581f2c56e5adf
FreightDH <maxim.zolotkov@yandex.ru>
2023-11-25 15:00:10 +0300

    feat: add brackets solution

---
+++ brackets/src/index.js
@@ -0,0 +1,18 @@
+module.exports = function check(str, bracketsConfig) {
+	let n = str.length; 
+
+	if (n % 2 !== 0) 
+		return false;
+
+	for (let i = 0; i < n; i++) {
+		bracketsConfig.forEach(bracket => {
+			bracket = bracket.join('');
+			if (str.includes(bracket)) {
+				str = str.replace(bracket, '');
+				i = 0;
+			}
+		})
+	}
+
+	return str.length ? false : true;
+}
